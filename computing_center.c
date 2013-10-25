b212af2aaa77cb868e34b7775453e241c44d6799
Locke Chen <locke12456@gmail.com>
2013-10-25 17:06:17 +0800

    [New features] create file computing_center.c

---
+++ computing_center.c
@@ -0,0 +1,30 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  computing_center.c
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  2013/10/25 下午 05:04:10
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  YOUR NAME (), 
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+
+void _init_computing_center()
+{
+}
+
+void computing()
+{
+}
+
+void computing_center()
+{
+
+}
