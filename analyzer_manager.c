075cb78b8cec72c7b888af1b4ff28a4d5780c071
Locke Chen <locke12456@gmail.com>
2013-10-25 15:12:47 +0800

    [New features] create file "  analyzer_manager.c"

---
+++ analyzer_manager.c
@@ -0,0 +1,23 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  analyzer_manager.c
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  2013/10/25 下午 03:11:02
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  YOUR NAME (), 
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+
+#include "hadoop_api.h"
+
+void analyzer_manager(){
+	//modified
+}
